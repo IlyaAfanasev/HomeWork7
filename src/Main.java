9adeaf33de5a0bf72e8efae62d22c07eda675865
IlyuAfanasev <gecktor.86@mail.ru>
2023-03-01 16:53:43 +0400

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
     }
+
+    public static void task1 () {
+        System.out.println("     Задача 1");
+        int salary = 15000;
+        int total = 0;
+        int mouth = 0;
+        while (total<2_459_000) {
+            total=total+salary;
+            mouth++;
+            System.out.println("Месяц "+mouth+ " сумма накоплений равна  "+total+"рублей");
+        }
+    }
+
+
 }
